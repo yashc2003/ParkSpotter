b5ca55c10526f00ac009f395f1ec06011407e68f
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-24 16:42:21 +0530

    Templates directory set

--- parkspotter/parkspotter/settings.py
+++ parkspotter/parkspotter/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'parkspotter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,""],
+        'DIRS': [BASE_DIR,"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
