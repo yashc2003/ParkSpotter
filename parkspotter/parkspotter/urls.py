6245f5134da5e83a07010c0362ef693920d007e5
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-24 16:56:17 +0530

    Url and views connected

--- parkspotter/parkspotter/urls.py
+++ parkspotter/parkspotter/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from parkspotter import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),#Site url
-    path('new_entry/', new_entry, name='new_entry'),#New entry url
+    path('', views.index, name='index'),#Site url
+    path('new_entry/', views.new_entry, name='new_entry'),#New entry url
 ]
