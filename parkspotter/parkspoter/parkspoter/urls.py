dd68a0bf51173e76684ea79503571a50b03da6c2
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-08-24 15:50:12 +0530

    Whole folder recreate

--- parkspotter/parkspoter/parkspoter/urls.py
+++ parkspotter/parkspoter/parkspoter/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Setup project.
+URL configuration for parkspoter project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,19 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from Setup import views
 
 urlpatterns = [
-    path('admin-panel/', admin.site.urls),
-    path("", views.index, name="index"),
-    path("login/", views.login_view, name="login"),
-    path("new-entry/", views.new_entry, name="new_entry"),
-    path("receipt/", views.receipt, name="receipt"),
-    path("user-registration/", views.user_registration, name="user_registration"),
-] 
+    path('admin/', admin.site.urls),
+]
