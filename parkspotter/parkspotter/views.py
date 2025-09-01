5b560106968ea23c368d3ce749eaee7f0db66535
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-01 20:09:19 +0530

    User imported

--- parkspotter/parkspotter/views.py
+++ parkspotter/parkspotter/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 @login_required
