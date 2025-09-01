2e03b91a60bae05895b0505c92f4f909e140b938
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-01 19:28:30 +0530

    Login required for dashboard added

--- parkspotter/parkspotter/views.py
+++ parkspotter/parkspotter/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     data={
         'title': 'Dashboard'
