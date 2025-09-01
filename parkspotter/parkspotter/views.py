67521eddf50ccf796f6f373f98b1d5b3d7a46f82
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-01 19:25:54 +0530

    Decoraters imported

--- parkspotter/parkspotter/views.py
+++ parkspotter/parkspotter/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     data={
