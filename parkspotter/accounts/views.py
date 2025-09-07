7a13e59699cf761183431577ebc9874728265e1f
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-07 16:03:59 +0530

    Make password imported

--- parkspotter/accounts/views.py
+++ parkspotter/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model, login, authenticate, logout
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
