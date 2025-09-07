25187c85bde50f432dffcfc1ef945a07f50d9e07
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-07 15:59:59 +0530

    str constuctor fired

--- parkspotter/accounts/models.py
+++ parkspotter/accounts/models.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import AbstractUserUser
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, unique=True)
     
-   
+    def __str__(self):
+        return self.username
