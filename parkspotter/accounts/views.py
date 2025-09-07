aa4567e3409b871da1ff1772b6bceb2a1f41c701
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-07 16:18:14 +0530

    Render to registration pg

--- parkspotter/accounts/views.py
+++ parkspotter/accounts/views.py
@@ -30,3 +30,4 @@ def signup_view(request):
         )
         messages.success(request, "Registration successful. Please log in.")
         return redirect('login')
+    return render(request, 'registration.html')
