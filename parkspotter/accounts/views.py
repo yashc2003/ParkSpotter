60e2796439076965e4107e94d6947785d573116b
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-07 16:04:52 +0530

    Msg imported

--- parkspotter/accounts/views.py
+++ parkspotter/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model, login, authenticate, logout
 from django.contrib.auth.hashers import make_password
+from django.contrib import messages
 
 # Create your views here.
