842fcdf75648e3d0b701a0bec178cde173720367
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-07 15:59:29 +0530

    CustomerUser class created

--- parkspotter/accounts/models.py
+++ parkspotter/accounts/models.py
@@ -2,3 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUserUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, unique=True)
+    
+   
