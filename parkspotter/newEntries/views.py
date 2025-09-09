8380d8ccb2a01c54526543084aca0f2905bdaf3f
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-09 15:08:08 +0530

    Data get if method=post

--- parkspotter/newEntries/views.py
+++ parkspotter/newEntries/views.py
@@ -8,3 +8,10 @@ from django.utils.dateparse import parse_datetime
 # Create your views here.
 
 @login_required
+def new_entry(request):
+    if request.method == 'POST':
+        plate_number = request.POST.get('plateNumber')
+        vehicle_type = request.POST.get('vehicleType')
+        entry_time = request.POST.get('entryTime')
+        exit_time = request.POST.get('exitTime')
+        is_paid = request.POST.get('isPaid') == 'on'
