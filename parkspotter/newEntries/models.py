78df57a036cfcf0f8b56a2bf93f22a023c07b9d5
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-09 11:29:43 +0530

    Vehicle type choices added

--- parkspotter/newEntries/models.py
+++ parkspotter/newEntries/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class NewEntry(models.Model):
+    VEHICLE_TYPE_CHOICES=[
+        ('Car','Car'),
+        ('Bike','Bike'),
+    ]
+    plate_number=models.CharField(max_length=20)
