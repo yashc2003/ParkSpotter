232836cde56c5efcbce96f10601ffbd015b0e877
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-11 20:20:52 +0530

    datetime imported

--- parkspotter/receipts/views.py
+++ parkspotter/receipts/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render,get_object_or_404
 from .models import NewEntry
 from django.contrin.b.auth.decorators import login_required
 from django.contrib import messages
-
+from datetime import datetime
 
 # Create your views here.
