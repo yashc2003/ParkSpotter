3163966ea2aac94e618930c6ea4fedf75559e4bd
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-11 11:02:22 +0530

    Path created for logout

--- parkspotter/accounts/urls.py
+++ parkspotter/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.signup_view, name='user_registration'),
     path('', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     
 ]
