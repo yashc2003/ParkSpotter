dc699f352fbb8830c38620f33fdfffea0eec00ac
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-11 19:57:40 +0530

    newEntry model updated

--- parkspotter/newEntries/models.py
+++ parkspotter/newEntries/models.py
@@ -9,7 +9,6 @@ class NewEntry(models.Model):
     plate_number=models.CharField(max_length=20)
     vehicle_type=models.CharField(max_length=10,choices=VEHICLE_TYPE_CHOICES)
     entry_time=models.DateTimeField()
-    exit_time=models.DateTimeField(null=True,blank=True)
     is_paid=models.BooleanField(default=False)
 
     def __str__(self):
