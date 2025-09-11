9d4d447835a44a8613c1f9b9a15917d659265ae1
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-11 20:19:53 +0530

    Msgs imported

--- parkspotter/receipts/views.py
+++ parkspotter/receipts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from .models import NewEntry
 from django.contrin.b.auth.decorators import login_required
+from django.contrib import messages
 
 
 # Create your views here.
